28c602b2e72c5986a436a24bbf9afdbfcd163603
sameerag <sameera910409@gmail.com>
2019-11-05 09:52:14 +0530

    Add environment variable support for user-mgt configuration
    Fix https://github.com/wso2/product-ei/issues/4724

--- core/org.wso2.carbon.base/src/main/java/org/wso2/carbon/base/ServerConfiguration.java
+++ core/org.wso2.carbon.base/src/main/java/org/wso2/carbon/base/ServerConfiguration.java
@@ -1,17 +1,17 @@
-/* 
+/*
  * Copyright 2005,2006 WSO2, Inc. http://www.wso2.org
- * 
- * Licensed under the Apache License, Version 2.0 (the "License"); 
- * you may not use this file except in compliance with the License. 
- * You may obtain a copy of the License at 
- * 
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
  * http://www.apache.org/licenses/LICENSE-2.0
- * 
+ *
  * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS, 
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. 
- * See the License for the specific language governing permissions and 
- * limitations under the License. 
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
  */
 package org.wso2.carbon.base;
 
@@ -105,7 +105,7 @@ public class ServerConfiguration implements ServerConfigurationService {
 
 	/**
 	 * Method to retrieve an instance of the server configuration.
-	 * 
+	 *
 	 * @return instance of the server configuration
 	 */
 	public static ServerConfiguration getInstance() {
@@ -122,10 +122,10 @@ public class ServerConfiguration implements ServerConfigurationService {
 	/**
 	 * This initializes the server configuration. This method should only be
 	 * called once, for successive calls, it will be checked.
-	 * 
+	 *
 	 * @param xmlInputStream
 	 *            the server configuration file stream.
-	 * 
+	 *
 	 * @throws org.wso2.carbon.base.ServerConfigurationException
 	 *             if the operation failed.
 	 */
@@ -161,10 +161,10 @@ public class ServerConfiguration implements ServerConfigurationService {
 	/**
 	 * This initializes the server configuration. This method should only be
 	 * called once, for successive calls, it will be checked.
-	 * 
+	 *
 	 * @param configurationXMLLocation
 	 *            the location of the server configuration file (carbon.xml).
-	 * 
+	 *
 	 * @throws ServerConfigurationException
 	 *             if the operation failed.
 	 */
@@ -228,10 +228,10 @@ public class ServerConfiguration implements ServerConfigurationService {
 	/**
 	 * Method to forcibly initialize the server configuration. If there is any
 	 * configuration loaded, it will not be preserved.
-	 * 
+	 *
 	 * @param xmlInputStream
 	 *            the server configuration file stream.
-	 * 
+	 *
 	 * @throws ServerConfigurationException
 	 *             if the operation failed.
 	 */
@@ -244,10 +244,10 @@ public class ServerConfiguration implements ServerConfigurationService {
 	/**
 	 * Method to forcibly initialize the server configuration. If there is any
 	 * configuration loaded, it will not be preserved.
-	 * 
+	 *
 	 * @param configurationXMLLocation
 	 *            the location of the server configuration file (carbon.xml).
-	 * 
+	 *
 	 * @throws ServerConfigurationException
 	 *             if the operation failed.
 	 */
@@ -259,12 +259,12 @@ public class ServerConfiguration implements ServerConfigurationService {
 
 	/**
 	 * Method to forcibly initialize the server configuration.
-	 * 
+	 *
 	 * @param configurationXMLLocation
 	 *            the location of the server configuration file (carbon.xml).
 	 * @param isLoadedConfigurationPreserved
 	 *            whether the currently loaded configuration is preserved.
-	 * 
+	 *
 	 * @throws ServerConfigurationException
 	 *             if the operation failed.
 	 */
@@ -328,8 +328,8 @@ public class ServerConfiguration implements ServerConfigurationService {
 		while (indexOfStartingChars < text.indexOf("${")
 				&& (indexOfStartingChars = text.indexOf("${")) != -1
 				&& (indexOfClosingBrace = text.indexOf('}')) != -1) { // Is a
-																		// property
-																		// used?
+			// property
+			// used?
 			String sysProp = text.substring(indexOfStartingChars + 2,
 					indexOfClosingBrace);
 			String propValue = System.getProperty(sysProp);
@@ -352,7 +352,7 @@ public class ServerConfiguration implements ServerConfigurationService {
 
 	/**
 	 * Method to a given key, value pair into the configuration.
-	 * 
+	 *
 	 * @param key
 	 *            the key to add
 	 * @param value
@@ -382,7 +382,7 @@ public class ServerConfiguration implements ServerConfigurationService {
 	/**
 	 * overrides the configuration property instead of adding the value to the
 	 * list
-	 * 
+	 *
 	 * @param key
 	 * @param value
 	 */
@@ -425,10 +425,10 @@ public class ServerConfiguration implements ServerConfigurationService {
 	/**
 	 * There can be multiple objects with the same key. This will return the
 	 * first String from them
-	 * 
+	 *
 	 * @param key
 	 *            the search key
-	 * 
+	 *
 	 * @return value corresponding to the given key
 	 */
 	@Override
@@ -442,10 +442,10 @@ public class ServerConfiguration implements ServerConfigurationService {
 
 	/**
 	 * There can be multiple object corresponding to the same object.
-	 * 
+	 *
 	 * @param key
 	 *            the search key
-	 * 
+	 *
 	 * @return the properties corresponding to the <code>key</code>
 	 */
 	@Override
@@ -459,7 +459,7 @@ public class ServerConfiguration implements ServerConfigurationService {
 
 	/**
 	 * Method to retrieve the Configuration as an XML Document.
-	 * 
+	 *
 	 * @return DOM element containing server configuration.
 	 */
 	@Override
@@ -484,10 +484,10 @@ public class ServerConfiguration implements ServerConfigurationService {
 
 	/**
 	 * Converts a given OMElement to a DOM Element.
-	 * 
+	 *
 	 * @param element
 	 *            the OM element to be converted to DOM.
-	 * 
+	 *
 	 * @return Returns Element.
 	 * @throws Exception
 	 *             if the operation failed.
